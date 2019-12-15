dbd22193e403b1376863b58c692e2c6f62c8d9ed
hassen <hasansaid51@gmail.com>
2019-12-15 11:15:16 -0800

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,173 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import pipboy from './pipboy.PNG'
+import connect from './connect4.PNG'
+import tom from './tom.PNG'
+import real from './realestate.PNG'
+import review from './gamereview.png'
+import clothing from './clothing.PNG'
+import { Header, Nav, Page, A, Name, Flextext, H1, innerText, Fortitle, P, Section, Image, Projects, Hr, One, Laptop, Pipboy, DivOne, Last, Btn1, Btn2 } from './styled.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <Header>
+        <Nav>
+          <Flextext>
+            <Name>Hassen Hassen</Name>
+            <innerText>
+              <A href="#projects">PORTFOLIO</A>
+              <A href="">CONTACT</A>
+            </innerText>
+          </Flextext>
+        </Nav>
+        <Fortitle>
+          <H1>(Software Developer)</H1>
+          <P>Hi, my Name is Hassen S. Hassen and I am a software Engineer with a background in customer service. my background in customer service allows me to communicate effectively with both clients and on a team. As a creative problem solver with a passion for learning and continually seek better ways to provide clients with innovative solutions.</P>
+        </Fortitle>
+      </Header>
+      <Section>
+        <h2>Skills</h2>
+        <Image src="https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png" />
+        <Image src="http://www.myiconfinder.com/uploads/iconsets/256-256-a25e146a3379d2f49b1247981f1120e2-css3.png" />
+        <Image src="https://www.sportsengineers.com/wp-content/uploads/2015/05/js-logo-badge-512.png" />
+        <Image src="https://images.tutorialedge.net/images/node.png" />
+        <Image src="https://cdn.spec-india.com/wp-content/uploads/2017/03/Express-JS.png" />
+        <Image src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" />
+        <Image src="https://miro.medium.com/max/500/1*55mb4NrRqKfI71p8WfYiTw.jpeg" />
+        <Image src="https://cdn.iconscout.com/icon/free/png-256/mongodb-5-1175140.png" />
+        <Image src="https://i.pinimg.com/originals/84/b1/06/84b1065e798f61aa80b8670a4b6fbb4d.png" />
+        <Image src="http://pngimg.com/uploads/linux/linux_PNG1.png" />
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png" />
+        <Image src="https://www.probytes.net/wp-content/uploads/2018/10/flask-logo-png-transparent.png" />
+        <Image src="https://www.getpostman.com/img/v2/logo-glyph.png" />
+        <Image src="https://vitworks.com/wp-content/uploads/2017/02/consuming-restful-api.png" />
+        <Image src="http://pluspng.com/img-png/bootstrap-png-bootstrap-512.png" />
+      </Section>
+      <Projects>
+        <Hr />
+        <h1 id="projects">Projects</h1>
+        <One>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={pipboy} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>Pipboy from Fallout</h3>
+              <p>Created with Bootstrap | JQuery</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </One>
+        <br />
+        <br />
+        <br />
+        <Hr />
+        <br />
+        <br />
+        <br />
+        <One>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={connect} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>Connect 4</h3>
+              <p>Created with HTML | CSS | JavaScript</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </One>
+        <br />
+        <br />
+        <br />
+        <Hr />
+        <br />
+        <br />
+        <br />
+        <One>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={tom} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>Tamagotchi</h3>
+              <p>Created with HTML | CSS | JavaScript</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </One>
+        <br />
+        <br />
+        <br />
+        <Hr />
+        <br />
+        <br />
+        <br />
+        <One>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={real} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>Real Estate</h3>
+              <p>Created with JavaScript | Express | Node | MongoDB | CSS | Embedded JavaScript (EJS) | HTML | CSS library called Skeleton</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </One>
+        <br />
+        <br />
+        <br />
+        <Hr />
+        <br />
+        <br />
+        <br />
+        <One>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={review} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>Legendary Game Reviews</h3>
+              <p>Group project created with React | React-Router | Styled-Components | React-Spinner | RAWG API | Font-Awesome | for Front-end | Python | Flask | Peewee | SQLite3/PostgreSQL | for Back-end</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </One>
+        <br />
+        <br />
+        <br />
+        <Hr />
+        <br />
+        <br />
+        <br />
+        <Last>
+          <div>
+            <Laptop src="https://rize.digital/wp-content/uploads/2017/09/macbook-white-screen.png" />
+            <Pipboy src={clothing} />
+          </div>
+          <DivOne>
+            <div>
+              <h3>E-Commerce Clothing Store</h3>
+              <p>Created with MERN stack with Firebase Authentication and Storage</p>
+              <Btn1><a src="">Link</a></Btn1>
+              <Btn2>Github</Btn2>
+            </div>
+          </DivOne>
+        </Last>
+      </Projects>
+    </Page>
   );
 }
-
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,183 @@
+import styled from 'styled-components'
+
+export const Page = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+  scroll-behavior: inherit;
+`
+export const Header = styled.header`
+  height: 75vh;
+  background-color: antiquewhite;
+`
+
+///////////////////////////////////////nav start
+export const Nav = styled.nav`
+  width: 100%:
+  height: 0px;
+  color: white;
+  background-color: black;
+`
+
+export const A = styled.a`
+  color: inherit;
+  text-decoration: none;
+  padding: 10px 20px;
+  position: relative;
+  top: 35px;
+  :hover {
+    color: red;
+  }
+`
+export const Flextext = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+export const Name = styled.div`
+  color: inherit;
+  font-size: 25px;
+  text-decoration: none;
+  padding: 25px;
+  :hover {
+    color: red;
+  }
+`
+///////////////////////////////////////nav end
+
+//////////////////////////////////////header title
+export const H1 = styled.h1`
+  color: white;
+  position: relative;
+  top: 30%;
+  font-size: 74.5px;
+`
+export const Fortitle = styled.div`
+  width: 50vw;
+  height: 40vh;
+  margin: 0 auto;
+  text-align: center;
+`
+
+export const P = styled.p`
+  position: relative;
+  top: 100px;
+  font-size: 18px;
+  font-weight: bold;
+`
+///////////////////////////////////////header end
+
+///////////////////////////////////////icons start
+
+export const Section = styled.section`
+  width: 100vw;
+  height: 35vh;
+  text-align: center;
+`
+
+export const Image = styled.img`
+  height: 96px;
+  width: 96px;
+  position: relative;
+  top: 42px;
+`
+
+///////////////////////////////////////icons end
+
+export const Hr = styled.hr`
+  width: 80vw;
+`
+
+///////////////////////////////////////Project1 start
+
+export const Projects = styled.div`
+  text-align: center;
+  height: 80vh;
+  scroll-behavior: smooth;
+`
+//////////////////////////////////////Project1 end
+
+//////////////////////////////////////one start
+export const One = styled.div`
+  width: 1180px;
+  height: 350px;
+  background-color: rgb(234,60,83);
+  margin: 0 auto;
+`
+
+export const Laptop = styled.img`
+  height: 300px;
+  width: 600.2px;
+  position: relative;
+  right: 10rem;
+  top: 1rem;
+`
+export const Pipboy = styled.img`
+  height: 240px;
+  width: 400px;
+  position: relative; 
+  right: 41.5rem;
+  bottom: 1.5rem;
+`
+
+export const DivOne = styled.div`
+  margin: 0;
+  width: 50%;
+  position: relative;
+  left: 29rem;
+  bottom: 17rem;
+`
+
+export const Btn1 = styled.button`
+  background-color: white;
+  color: black;
+  border: 2px solid black;
+  height: 30px;
+  width: 93px;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 20px;
+  margin-top: 6rem;
+  margin-right: 9rem;
+&:hover {
+  background-color: black;
+  color: white;
+}
+&:focus {
+  outline: none;
+}
+font-size: 20px;
+`
+
+export const Btn2 = styled.button`
+background-color: white;
+  color: black;
+  border: 2px solid black;
+  height: 30px;
+  width: 93px;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 20px;
+
+
+&:hover {
+  background-color: black;
+  color: white;
+}
+&:focus {
+  outline: none;
+}
+font-size: 20px;
+`
+//////////////////////////////////////one end
+
+/////////////////////////////////////last start
+
+export const Last = styled.div`
+width: 1180px;
+height: 350px;
+background-color: rgb(234,60,83);
+margin: 0 auto 2rem auto;
+`
+
+////////////////////////////////////last end
