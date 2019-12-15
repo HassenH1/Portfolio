6c73ee792ce999dbdba5d2f06e5c2bb6af427017
hassen <hasansaid51@gmail.com>
2019-12-15 11:49:29 -0800

    Updates

---
+++ precache-manifest.6a8a6fa321ce4d8e5ce884921377abad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2542064ceaf093cc0f842d73de0062ef",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1491b62b1558fe54bf5e",
+    "url": "/Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d237b382908ad97593f7",
+    "url": "/Portfolio/static/js/2.ac48c051.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Portfolio/static/js/2.ac48c051.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1491b62b1558fe54bf5e",
+    "url": "/Portfolio/static/js/main.b66652a5.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  },
+  {
+    "revision": "77445f35780fcb71aabc8633f48b3910",
+    "url": "/Portfolio/static/media/clothing.77445f35.PNG"
+  },
+  {
+    "revision": "bc053b90aa425652eb45c556f2c836a4",
+    "url": "/Portfolio/static/media/connect4.bc053b90.PNG"
+  },
+  {
+    "revision": "c45524675e4e68419e16d58ceefd44b0",
+    "url": "/Portfolio/static/media/gamereview.c4552467.png"
+  },
+  {
+    "revision": "e30f8bef1ab7c0e5ae313ed9728d804a",
+    "url": "/Portfolio/static/media/pipboy.e30f8bef.PNG"
+  },
+  {
+    "revision": "0d38d6a7b0f243d7239eb562ee26567d",
+    "url": "/Portfolio/static/media/realestate.0d38d6a7.PNG"
+  },
+  {
+    "revision": "0585e92162a24d5b2600fa506e29adc5",
+    "url": "/Portfolio/static/media/tom.0585e921.PNG"
+  }
+]);
